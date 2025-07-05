e7c49e71afb36286b637676f6f8817e5f68a07cf
arsagyr <sdkalsky@gmail.com>
2025-07-06 02:59:37 +0700

    Робит всё и на Реакт

--- backend/internal/handlers/delete.go
+++ backend/internal/handlers/delete.go
@@ -8,34 +8,7 @@ import (
 	"react/backend/internal/model"
 	"strconv"
 	"strings"
-
-	"github.com/gorilla/mux"
 )
-
-func DeleteHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	var nameid int
-	row := DB.QueryRow(`
-	SELECT Nameid FROM Actors 
-	WHERE id=$1
-	`, id)
-	err := row.Scan(&nameid)
-	if err != nil {
-		log.Println(err)
-	}
-	_, err = DB.Exec("DELETE FROM Actors WHERE id = $1", id)
-	if err != nil {
-		log.Println(err)
-	}
-
-	_, err = DB.Exec("DELETE FROM Names WHERE id = $1", nameid)
-	if err != nil {
-		log.Println(err)
-	}
-
-	http.Redirect(w, r, "/", 301)
-}
 
 func DeleteActorHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
